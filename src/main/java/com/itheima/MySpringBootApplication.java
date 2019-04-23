5d1e9cc2a7346644147ab7a3a790e3d9af2e82eb
wyb <1911260010@qq.com>
2019-04-23 19:03:25 +0800

    将springboot1提交至git

---
+++ src/main/java/com/itheima/MySpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: wyb
+ * @Date: 2019/4/22 20:50
+ * @Description:
+ */
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class);
+    }
+}
---
+++ src/main/java/com/itheima/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: wyb
+ * @Date: 2019/4/22 20:53
+ * @Description:
+ */
+@RestController
+public class TestController {
+    @RequestMapping("/test1")
+    public String test1(){
+        return "hello world";
+    }
+
+}
